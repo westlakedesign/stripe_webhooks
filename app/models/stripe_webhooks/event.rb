2a1c4d69a4a6cbc15ccd3cbac352f088f3f0d99d
Greg Woods <greg@westlakedesign.com>
2015-07-15 13:04:32 -0400

    add a method to process events by fetching them from the stripe event api

--- app/models/stripe_webhooks/event.rb
+++ app/models/stripe_webhooks/event.rb
@@ -1,5 +1,25 @@
 module StripeWebhooks
   class Event < ActiveRecord::Base
     validates :stripe_event_id, :presence => true, :uniqueness => true
+
+    def stripe_event
+      @_stripe_event ||= Stripe::Event.retrieve(stripe_event_id)
+      return @_stripe_event
+    end
+
+    def process!
+      begin
+        event = stripe_event()
+        update_attributes({
+          :is_processed => true,
+          :is_authentic => true,
+          :stripe_event_type => event.type,
+          :stripe_created_at => Time.at(event.created).to_datetime
+        })
+      rescue Stripe::InvalidRequestError
+        update_attributes(:is_processed => true, :is_authentic => false)
+      end
+    end
+
   end
 end
