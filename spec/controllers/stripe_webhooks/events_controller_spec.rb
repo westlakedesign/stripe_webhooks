a604219f232f9b3adff08b265836bfb8e30a21d4
Greg Woods <greg@westlakedesign.com>
2015-07-17 12:10:50 -0400

    controller tests

--- spec/controllers/stripe_webhooks/events_controller_spec.rb
+++ spec/controllers/stripe_webhooks/events_controller_spec.rb
@@ -5,26 +5,42 @@ module StripeWebhooks
 
     routes { StripeWebhooks::Engine.routes }
 
+    let(:customer_created){
+      StripeMock.mock_webhook_payload('customer.created').to_json
+    }
+
     describe "POST #create" do
 
       it "should create the event" do
-        get :create
-        #expect(response).to have_http_status(:success)
+        expect{
+          post :create, customer_created
+        }.to change(StripeWebhooks::Event, :count).by(1)
+        expect(response).to have_http_status(:success)
       end
 
       it "should not create a duplicate event" do
-        get :create
-        #expect(response).to have_http_status(:success)
+        post :create, customer_created
+        expect{
+          post :create, customer_created
+        }.to_not change(StripeWebhooks::Event, :count)
+        expect(response).to have_http_status(:success)
       end
 
       it "should queue an active job" do
-        get :create
-        #expect(response).to have_http_status(:success)
+        expect{
+          post :create, customer_created
+        }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :length).by(1)
+        expect(ActiveJob::Base.queue_adapter.enqueued_jobs.last[:job]).to eq(StripeWebhooks::ProcessorJob)
       end
 
-      it "should return an error response" do
-        get :create
-        #expect(response).to have_http_status(:success)
+      it "should return an 400 error when the JSON is poorly formed" do
+        post :create, File.read(StripeWebhooks::Engine.root.join('spec', 'data', 'parse_error.json'))
+        expect(response).to have_http_status(400)
+      end
+
+      it "should return a 422 error when the provided data is not valid" do
+        post :create, File.read(StripeWebhooks::Engine.root.join('spec', 'data', 'invalid_data.json'))
+        expect(response).to have_http_status(422)
       end
     end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,9 @@ require 'rspec/rails'
 require 'database_cleaner'
 require "factory_girl_rails"
 require 'stripe_mock'
+require 'simplecov'
+
+SimpleCov.start 'rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
