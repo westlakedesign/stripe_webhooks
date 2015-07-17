58957e156fa1d475488509f515bf46186be343d0
Greg Woods <greg@westlakedesign.com>
2015-07-17 14:57:00 -0400

    register_callback spec

--- spec/lib/stripe_webhooks/callbacks_spec.rb
+++ spec/lib/stripe_webhooks/callbacks_spec.rb
@@ -4,7 +4,12 @@ module StripeWebhooks
   RSpec.describe Callbacks do
 
     describe '.register_callback' do
-      it 'should append the label to the list of callbacks'
+      it 'should append the label to the list of callbacks' do
+        expect{
+          StripeWebhooks.register_callback('hello_world')
+        }.to change(StripeWebhooks.callbacks, :length).by(1)
+        expect(StripeWebhooks.callbacks.last).to eq('hello_world')
+      end
     end
 
   end
