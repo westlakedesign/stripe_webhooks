3929b32181bea7159c2038d8e26ed72788a8aff2
Greg Woods <greg.woods@moserit.com>
2017-02-02 12:06:01 -0500

    Delete extra migrations

--- spec/dummy/db/migrate/20170201180822_add_user_id_to_stripe_webhook_events.stripe_webhooks.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from stripe_webhooks (originally 20170201175643)
-class AddUserIdToStripeWebhookEvents < ActiveRecord::Migration[5.0]
-  def change
-    change_table :stripe_webhooks_events do |t|
-      t.string :user_id
-      t.boolean :livemode, default: false
-    end
-  end
-end
